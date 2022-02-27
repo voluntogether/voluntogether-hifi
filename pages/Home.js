e4d661d4d723426bb4bfe576915a46f08c6cc290
Adam Siwiec <asiwiec@nvidia.com>
2022-02-27 00:39:50 -0800

    working

--- pages/Home.js
+++ pages/Home.js
@@ -1,9 +1,24 @@
 import React from "react";
-import { View, Text } from 'react-native-ui-lib';
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+// import { View, Text } from 'react-native-ui-lib';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 
-let Home = () => {
 
+let Home = ({ navigation }) => {
+
+
+
+    return (
+        <View>
+            <Text>Home</Text>
+            <FontAwesome5 name={'check'}
+                size={30}
+                color={"#000"} />
+
+
+        </View>
+    )
 };
 
 
