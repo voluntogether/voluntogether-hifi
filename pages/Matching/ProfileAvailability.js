a21c46e12f4908d190660d1ccf4c712737d15e46
Daniel Wan Rosli <danielwanrosli@DNa1ca81f.SUNet>
2022-03-03 05:38:04 -0800

    Buttons to availability

--- pages/Matching/ProfileAvailability.js
+++ pages/Matching/ProfileAvailability.js
@@ -1,7 +1,7 @@
 import React from "react";
-import _ from "underscore";
-import { View, Text, Image, Button, Picker } from "react-native-ui-lib";
-
+import _, { map } from "underscore";
+import { View, Text, Image, Button, Picker, TouchableOpacity } from "react-native-ui-lib";
+import Svg, { Path, G, Rect, Circle } from 'react-native-svg';
 
 
 let ProfileVolunteerAreas = ({ navigation }) => {
@@ -30,36 +30,42 @@ let ProfileVolunteerAreas = ({ navigation }) => {
         'Sat afternoon',
         'Sun afternoon']
 
+
+
+        const [selected, setSelected] = React.useState([]);
+        const handlePress = time =>
+        selected.includes(time) ? setSelected(selected.filter(s => s !== time)) : setSelected([...selected, time]);
+
+
     return (
-        <View>
-            <Text> Make Your Profile </Text>
-            <Text> Tap to select the times that you are generally free.</Text>
-            {/* <Image source={require('/assets/blobs/match-3-blob.svg')}/> */}
+        <View flex padding-page>
+            <Text heading center nonBlackBlack marginB-s4> Make Your Profile </Text>
+            <Text center={true} body fadedSubtext marginB-s4  marginT-s6> Tap to select the times that you are generally free.</Text>
 
+        <View center marginB-s4>
+            <Svg width="339" height="154" viewBox="0 0 339 154" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <G filter="url(#filter0_d_192_7040)">
+            <Rect x="4" width="331" height="146" rx="12" fill="#A4C1E3"/>
+            </G>
+            <Circle cx="74.5" cy="75.5" r="33.5" fill="#C1E3A4"/>
+            <Circle cx="266.5" cy="75.5" r="33.5" fill="#A4E3C6"/>
+            <Circle cx="170.5" cy="75.5" r="33.5" fill="#EAF3D7"/>
+            </Svg>
+        </View>
 
-            {/* <Picker
-                placeholder={'Placeholder'}
-                onChange={() => console.log('changed')}>
-                {
-                    times.map((item, index) => <Picker.Item index={index} value={item} />)}
-            </Picker> */}
-
+        <Button label={'next (temp)'} onPress={() => navigation.navigate('MatchingAlgorithm', {
+            rematch: false
+        })}/>
+            
             {_.map(times, time => (
-                <Text key={time.value}>{time}</Text>
-            ))}
+                <View marginT-s3>
+                    <Button fullWidth={false} size={Button.sizes.xSmall} onPress={() => handlePress(time)} backgroundColor={selected.includes(time) ? 'gray' : 'white'} outlineColor={'black'}>
+                        <Text key={time.value}>{time}</Text>
+                    </Button>
+                </View>
+                ))}     
 
-            <Picker
-                marginT-20
-                placeholder="Favorite Languages (up to 3)"
-                value={"abc"}
-                onChange={items => this.setState({ languages: items })}
-                mode={Picker.modes.MULTI}
-                selectionLimit={3}
-            >
-                {_.map(options, option => (
-                    <Picker.Item key={option.value} value={option} disabled={option.disabled} />
-                ))}
-            </Picker>
+
         </View >
     );
 }
