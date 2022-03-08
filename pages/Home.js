1f7175d5d676db0b153d89d538f67f7881c42cc3
Evan Baldonado <21182871+evanbaldonado@users.noreply.github.com>
2022-03-08 02:10:58 -0800

    Style explore button (in progress)

--- pages/Home.js
+++ pages/Home.js
@@ -37,15 +37,12 @@ let Home = ({ navigation }) => {
 
 
             <View centerH>
-                <Button centerH style={[Styles.invisibleBackgroundButton]} onPress={() => navigation.navigate('Matching')}>
+                <Button centerH style={[Styles.invisibleBackgroundButton]} onPress={() => navigation.navigate('Challenges')}>
                 <Card style={[Styles.yellowCard, Styles.boxShadow]} paddingL-s8 paddingR-s8 >
-                    <Text bigBody nonBlackBlack marginB-s4>Explore a new challenge</Text>
-                    <Image right style={{ width: 50, height: 50, float: "right" }} source={require("../assets/images/pencil.png")} />
-                    <Svg width="15" height="10" viewBox="0 0 27 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <Circle cx="194.461" cy="164.461" r="50.4614" fill="#F6CA44" />
-                        <Path fill-rule="evenodd" clip-rule="evenodd" d="M180.176 204.384C211.255 204.384 236.45 179.19 236.45 148.11C236.45 143.991 236.007 139.974 235.167 136.106C241.296 144.361 244.923 154.586 244.923 165.658C244.923 193.061 222.708 215.276 195.305 215.276C181.603 215.276 169.199 209.722 160.22 200.743C166.422 203.096 173.148 204.384 180.176 204.384Z" fill="#FAE269" />
-                        <Circle cx="82.4614" cy="72.4614" r="50.4614" fill="#F6CA44" />
-                    </Svg>
+                    <View style={{ flexDirection: "row", justifyContent: "space-around" }}>
+                        <Text bigBody nonBlackBlack marginB-s4>Explore a new challenge</Text>
+                        <Image marginL-s8 right style={{ width: 20, height: 20}} source={require("../assets/images/pencil.png")} />
+                    </View>
                 </Card>
                 </Button>
             </View>
