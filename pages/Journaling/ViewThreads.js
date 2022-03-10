adf714ed2d5a9342234ce5fca076794911b981cb
Evan Baldonado <21182871+evanbaldonado@users.noreply.github.com>
2022-03-10 02:20:17 -0800

    Remove extraneous content and add formatting

--- pages/Journaling/ViewThreads.js
+++ pages/Journaling/ViewThreads.js
@@ -18,40 +18,30 @@ let ViewThreads = ({ navigation, route }) => {
           <BackArrow navigation={navigation} />
           <Text heading center nonBlackBlack marginB-s4>{journal.organization}</Text>
 
+
       {journal.prompts.length > 0 ? _.map(journal.prompts, (prompt, index) => {
         return (
           // If there IS a thread entry!!!!
-          <Pressable onPress={() => navigation.navigate("ExpandThread", {
+          <View centerH><Pressable centerH onPress={() => navigation.navigate("ExpandThread", {
             id: id,
             index: index
           })}>
 
-            <View style={[Styles.journalBlankCard]} >
-              <Card style={[Styles.blueCardPrompt]} >
+              <Card style={[Styles.blueCardPrompt]} centerH>
                 <View centerH>
                   <FontAwesome5 name={'mountain'} size={30} color={"#000"}/>
-                  <Text bigBody marginT-s4>{prompt.prompt}</Text>
+                  <Text bigBody marginT-s4>Prompt: {prompt.prompt}</Text>
                 </View>
               </Card>
 
-            <View style={[Styles.alignRow]}>
-              <View style={[Styles.alignRow]}centerH >
-                {_.map(journal.users, (userID) => {
-                return (<ProfilePic id={userID} />)
-                })}
-                <Text>Created by King Bean</Text>
-              </View>
-
-            </View>
 
 
-            </View>
 
 
 
             {/* <View> <ProfilePic id={userID} /> </View> */}
 
-          </Pressable>)
+          </Pressable></View>)
 
       }) :
 
