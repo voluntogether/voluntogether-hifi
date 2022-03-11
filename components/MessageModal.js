0057280cbbc02389851354b8543653af46f638d5
Adam Siwiec <asiwiec@nvidia.com>
2022-03-11 06:18:39 -0800

    Error prevention for response n reflection

--- components/MessageModal.js
+++ components/MessageModal.js
@@ -1,35 +1,66 @@
 import React, { useState } from "react";
-import { Pressable  } from "react-native";
-import { Modal, TextField, Image, Text, Card, View} from "react-native-ui-lib"
+import { Pressable, Alert } from "react-native";
+import { Modal, TextField, Image, Text, Card, View } from "react-native-ui-lib"
 import { launchImageLibrary } from "react-native-image-picker";
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import Styles from "../Style.js";
 import _ from "underscore"
 let MessageModal = ({ prompt, openMessageModal, onChangeText, onDone, onCancel, images, imageCallback }) => {
 
+    const confirmSendReflection = () =>
+        Alert.alert(
+            "Send Reflection?",
+            "Once a reflection is posted it cannot be deleted.",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { text: "Send Response", onPress: () => onDone() }
+            ]
+        );
+
+    const confirmExitReflection = () =>
+        Alert.alert(
+            "Discard Reflection?",
+            "Your reflection will not be saved.",
+            [
+                {
+                    text: "Discard Reflection",
+                    onPress: () => onCancel(),
+                    style: "cancel"
+                },
+                {
+                    text: "Cancel", onPress: () => console.log("OK Pressed"),
+                    style: "cancel"
+                }
+            ]
+        );
+
 
     return (
         <Modal visible={openMessageModal} onBackgroundPress={() => console.log('background pressed')}>
             <Modal.TopBar
                 title="New Message"
-                onCancel={onCancel}
-                onDone={onDone}
+                onCancel={confirmExitReflection}
+                onDone={confirmSendReflection}
                 cancelIcon={null}
                 cancelLabel="Back"
             />
 
-            <View centerH> 
-            <Card marginT-s8 style={[Styles.blueCardThreadPrompt]} centerH>
-                <View centerH>
-                    <FontAwesome5 name={prompt.icon} size={30} color={"#ffffff"}/>
-                  <Text center bigBody marginT-s4>Prompt: {prompt.prompt}</Text>
-                </View>
-            </Card>
-            </View> 
+            <View centerH>
+                <Card marginT-s8 style={[Styles.blueCardThreadPrompt]} centerH>
+                    <View centerH>
+                        <FontAwesome5 name={prompt.icon} size={30} color={"#ffffff"} />
+                        <Text center bigBody marginT-s4>Prompt: {prompt.prompt}</Text>
+                    </View>
+                </Card>
+            </View>
 
             <FontAwesome5 name={prompt.icon} size={20} color={"white"} />
-            
-           <View marginT-s4 marginH-s8> 
+
+            <View marginT-s4 marginH-s8>
                 <Pressable onPress={() => {
                     launchImageLibrary({
                         mediaType: 'photo',
@@ -42,21 +73,21 @@ let MessageModal = ({ prompt, openMessageModal, onChangeText, onDone, onCancel, 
                     })
                 }}>
 
-                <View > 
-                <FontAwesome5 name={'image'} size={20} />
-                    {_.map(images, (image) => {
-                        return <Image source={{ uri: image.uri }} style={{ width: 50, height: 50 }} />
-                })}
+                    <View >
+                        <FontAwesome5 name={'image'} size={20} />
+                        {_.map(images, (image) => {
+                            return <Image source={{ uri: image.uri }} style={{ width: 50, height: 50 }} />
+                        })}
 
-                <TextField
-                    placeholder={'Enter your new message'}
-                    floatingPlaceholder
-                    onChangeText={onChangeText}
+                        <TextField
+                            placeholder={'Enter your new message'}
+                            floatingPlaceholder
+                            onChangeText={onChangeText}
 
-                />
-                </View>
-                
-                    
+                        />
+                    </View>
+
+
                 </Pressable>
             </View>
 
--- components/ReplyModal.js
+++ components/ReplyModal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Alert } from "react-native";
 import { Modal, TextField, Text, View } from "react-native-ui-lib"
 import ProfilePic from "./ProfilePic";
 import Styles from "../Style.js";
@@ -6,31 +7,62 @@ import Styles from "../Style.js";
 
 let ReplyModal = ({ message, openReplyModal, onChangeText, onDone, onCancel }) => {
 
+    const confirmSendResponse = () =>
+        Alert.alert(
+            "Send Response?",
+            "Once a response is posted it cannot be deleted.",
+            [
+                {
+                    text: "Cancel",
+                    onPress: () => console.log("Cancel Pressed"),
+                    style: "cancel"
+                },
+                { text: "Send Response", onPress: () => onDone() }
+            ]
+        );
+
+    const confirmExitResponse = () =>
+        Alert.alert(
+            "Discard Response?",
+            "Your response will not be saved.",
+            [
+                {
+                    text: "Discard Response",
+                    onPress: () => onCancel(),
+                    style: "cancel"
+                },
+                {
+                    text: "Cancel", onPress: () => console.log("OK Pressed"),
+                    style: "cancel"
+                }
+            ]
+        );
+
 
     return (
         <Modal visible={openReplyModal}>
             <Modal.TopBar
                 title="New Reply"
-                onCancel={onCancel}
-                onDone={onDone}
+                onCancel={confirmExitResponse}
+                onDone={confirmSendResponse}
                 cancelIcon={null}
                 cancelLabel="Back"
-    
-            />
-            <View marginT-s10 marginH-s8 > 
-            <View style={[Styles.alignRow]}>
-                <ProfilePic marginR-s4 id={message.message.user}/>
-                <Text marginL-s2 marginH-s8>{message.message.body}</Text>
-                
-            </View> 
 
-            <TextField
-                placeholder={'Enter your response'}
-                floatingPlaceholder
-                onChangeText={(message) => onChangeText(message)}
             />
-            
-            
+            <View marginT-s10 marginH-s8 >
+                <View style={[Styles.alignRow]}>
+                    <ProfilePic marginR-s4 id={message.message.user} />
+                    <Text marginL-s2 marginH-s8>{message.message.body}</Text>
+
+                </View>
+
+                <TextField
+                    placeholder={'Enter your response'}
+                    floatingPlaceholder
+                    onChangeText={(message) => onChangeText(message)}
+                />
+
+
             </View>
         </Modal>
 
