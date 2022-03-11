b63cb0897b2b4b6f57dcbc13b465a6f9fd181278
Cameron <73369938+cameronines@users.noreply.github.com>
2022-03-11 02:41:29 -0800

    msgmodal styling

--- components/MessageModal.js
+++ components/MessageModal.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Pressable } from "react-native";
-import { Modal, TextField, Image, Text } from "react-native-ui-lib"
+import { Pressable  } from "react-native";
+import { Modal, TextField, Image, Text, Card, View} from "react-native-ui-lib"
 import { launchImageLibrary } from "react-native-image-picker";
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import Styles from "../Style.js";
 import _ from "underscore"
 let MessageModal = ({ prompt, openMessageModal, onChangeText, onDone, onCancel, images, imageCallback }) => {
 
@@ -17,30 +18,47 @@ let MessageModal = ({ prompt, openMessageModal, onChangeText, onDone, onCancel, 
                 cancelLabel="Back"
             />
 
-            <Text>{prompt.prompt}</Text>
+            <View centerH> 
+            <Card marginT-s8 style={[Styles.blueCardThreadPrompt]} centerH>
+                <View centerH>
+                    <FontAwesome5 name={prompt.icon} size={30} color={"#ffffff"}/>
+                  <Text center bigBody marginT-s4>Prompt: {prompt.prompt}</Text>
+                </View>
+            </Card>
+            </View> 
+
             <FontAwesome5 name={prompt.icon} size={20} color={"white"} />
-            <TextField
-                placeholder={'Enter your new message'}
-                floatingPlaceholder
-                onChangeText={onChangeText}
+            
+           <View marginT-s4 marginH-s8> 
+                <Pressable onPress={() => {
+                    launchImageLibrary({
+                        mediaType: 'photo',
 
-            />
-            <Pressable onPress={() => {
-                launchImageLibrary({
-                    mediaType: 'photo',
+                    }, ({ didCancel, errorCode, errorMessage, assets }) => {
+                        console.log(didCancel, errorCode, errorMessage, assets)
+                        if (!didCancel) {
+                            imageCallback(assets)
+                        }
+                    })
+                }}>
 
-                }, ({ didCancel, errorCode, errorMessage, assets }) => {
-                    console.log(didCancel, errorCode, errorMessage, assets)
-                    if (!didCancel) {
-                        imageCallback(assets)
-                    }
-                })
-            }}>
+                <View > 
                 <FontAwesome5 name={'image'} size={20} />
-                {_.map(images, (image) => {
-                    return <Image source={{ uri: image.uri }} style={{ width: 50, height: 50 }} />
+                    {_.map(images, (image) => {
+                        return <Image source={{ uri: image.uri }} style={{ width: 50, height: 50 }} />
                 })}
-            </Pressable>
+
+                <TextField
+                    placeholder={'Enter your new message'}
+                    floatingPlaceholder
+                    onChangeText={onChangeText}
+
+                />
+                </View>
+                
+                    
+                </Pressable>
+            </View>
 
         </Modal>
 
